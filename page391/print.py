06df9a90899cf857e84d24d35d5f22aaf14965c0
miyazi777 <m71203@gmail.com>
2017-10-26 16:57:27 +0900

    add print func

---
+++ page391/print.py
@@ -0,0 +1,23 @@
+print('文字列') # 文字列
+
+x = 'text'
+print(x)    # text
+
+y = 100
+print(y)    # 100
+
+print(x, y) # text 100
+
+# 改行させない
+print('aaa', end='')    # aaa
+print('bbb', end='')    # aaabbb
+
+# 文字列結合
+x1 = 'text'
+x2 = 'moji'
+print(x1 + ' ' + x2)    # text moji
+
+# 数値を文字列に変換
+x = 'text'
+y = 100
+print(x + str(y))   # text100
