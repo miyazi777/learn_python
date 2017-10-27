5aaa761bdc1eae08d3c10c4eff5c8fee589e515f
miyazi777 <m71203@gmail.com>
2017-10-27 10:57:49 +0900

    add variable_num and calc

---
+++ calc.py
@@ -0,0 +1,30 @@
+# 四則演算
+x = 16
+y = 3
+val = x + y
+print(val)  # 19
+val = x - y
+print(val)  # 13
+val = x * y
+print(val)  # 48
+val = x / y
+print(val)  # 5.3333
+val = x // y
+print(val)  # 5
+val = x % y
+print(val)  # 1
+val = int(x / y)
+print(val)  # 5
+val = float(x)
+print(val)  # 16.0
+
+# 複素数
+val = 3 + 5j
+print(val.conjugate())  # (3-5j)
+
+# 乗数
+val = pow(2, 3)
+print(val)  # 8
+val = 2 ** 3
+print(val)  # 8
+
---
+++ variable_num.py
@@ -0,0 +1,24 @@
+# int
+num = 5
+print(num)  # 5
+bin_val = 0b0101
+print(bin_val)  # 5
+oct_val = 0o165
+print(oct_val)  # 117
+hex_val = 0xfaa
+print(hex_val)  # 4010
+
+# float
+x = 0.0012
+print(x)    # 0.0012
+y = 1.2e-3
+print(y)    # 0.0012
+
+# complex
+c1 = 2 + 3j
+print(c1)   # (2+3j)
+c2 = complex(5, 6)
+print(c2)   # (5+6j)
+
+print(c1.real)  # 2.0
+print(c2.imag)  # 6.0
