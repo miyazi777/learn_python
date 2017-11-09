e06db15486f68935fc45e561ade0b5f84543dbf9
miyazi777 <m71203@gmail.com>
2017-11-09 13:49:50 +0900

    add format and type sample

---
+++ format.py
@@ -0,0 +1,4 @@
+total = 35
+avg = 17.5
+print('total {:d}'.format(total))   # total 35
+print('avg {:2.2f}'.format(avg))    # avg 17.50
---
+++ type.py
@@ -0,0 +1,5 @@
+print(type(4))      # <calss 'int'>
+print(type(2.5))    # <class 'float'>
+print(type('abc'))  # <class 'str'>
+print(type([1,2,3]))# <class 'list'>
+print(type({'a':1}))# <class 'dict'>
